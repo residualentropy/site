7a5d395ce5034f237fe34e35985bca026afec88f
residualentropy <161914158+residualentropy@users.noreply.github.com>
2024-09-23 18:20:23 -0500

    hAve client (pi) aggregate each period

--- main.py
+++ main.py
@@ -13,17 +13,14 @@ app = Flask(__name__)
 def root():
 	return '<h1>Hooray!</h1>'
 
-geiger_this_period = 0
 geiger_cpm = collections.deque()
 SAVED_DURATION_SECS = 50
 CPM_PERIOD_SECS = 5
-period_start_time = time.time()
-in_startup_period = True
 
 class Entry:
 	__slots__ = ['ts_start', 'cpm']
 
-	def __init__(self, ts_start: float, cpm: int):
+	def __init__(self, ts_start: float, cpm: float):
 		self.ts_start = ts_start
 		self.cpm = cpm
 	
@@ -38,28 +35,15 @@ def geiger_got(content_b64):
 	if request.headers['X-Shared-Secret'] != secret:
 		return "err, bad secret", 401
 	content = b64decode(content_b64.encode('utf-8')).decode('utf-8')
-	num = int(content.strip())
-	now = time.time()
-	msg = ''
-	global period_start_time, geiger_this_period, in_startup_period
-	if (now - period_start_time) > CPM_PERIOD_SECS:
-		msg += 'period_end: ('
-		if in_startup_period:
-			in_startup_period = False
-			msg += 'in_startup_period'
-		else:
-			cps = geiger_this_period / CPM_PERIOD_SECS
-			cpm = cps * 60
-			geiger_cpm.append(Entry(period_start_time, cpm))
-			msg += f'saved_period: cpm: {cpm}'
-		geiger_this_period = 0
-		period_start_time = now
-		msg += '), '
-	geiger_this_period += num
-	msg += f'recieved_count {num}'
+	[period_start_time, cpm] = [
+		float(i)
+		for i in content.strip().split(',')
+	]
+	geiger_cpm.append(Entry(period_start_time, cpm))
+	msg = f'recieved: (cpm: {cpm} period_start_time: {period_start_time})'
 	n_purged = 0
 	while len(geiger_cpm) > 0:
-		if geiger_cpm[0].ts_start < (now - SAVED_DURATION_SECS):
+		if geiger_cpm[0].ts_start < (period_start_time - SAVED_DURATION_SECS):
 			geiger_cpm.popleft()
 			n_purged += 1
 		else:
